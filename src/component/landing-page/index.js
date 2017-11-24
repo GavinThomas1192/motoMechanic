a68a903f7d86c6f7c916f4cb2b894309287cdeaa
GavinThomas1192 <gthomas1192@gmail.com>
2017-11-24 11:58:45 -0800

    working on firebase

--- src/component/landing-page/index.js
+++ src/component/landing-page/index.js
@@ -38,4 +38,41 @@ let mapDispatchToProps = dispatch => ({
 });
 
 
-export default connect(mapStateToProps, mapDispatchToProps)(LandingPage);
+// export default connect(mapStateToProps, mapDispatchToProps)(LandingPage);
+
+// import React from 'react';
+// import { FirebaseAuth } from 'react-firebaseui';
+// import firebase from 'firebase';
+
+// // Configure Firebase.
+// const config = {
+//     apiKey: 'AIzaSyAeue-AsYu76MMQlTOM-KlbYBlusW9c1FM',
+//     authDomain: 'myproject-1234.firebaseapp.com',
+//     // ...
+// };
+// firebase.initializeApp(config);
+
+// // Configure FirebaseUI.
+// const uiConfig = {
+//     // Popup signin flow rather than redirect flow.
+//     signInFlow: 'popup',
+//     // Redirect to /signedIn after sign in is successful. Alternatively you can provide a callbacks.signInSuccess function.
+//     signInSuccessUrl: '/signedIn',
+//     // We will display Google and Facebook as auth providers.
+//     signInOptions: [
+//         firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+//         firebase.auth.FacebookAuthProvider.PROVIDER_ID
+//     ]
+// };
+
+// class SignInScreen extends React.Component {
+//     render() {
+//         return (
+//             <div>
+//                 <h1>My App</h1>
+//                 <p>Please sign-in:</p>
+//                 <FirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()} />
+//             </div>
+//         );
+//     }
+// }
