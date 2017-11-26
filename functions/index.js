4e82b93c6fc4d6eaa1c45384abb3dd3e67da5f81
GavinThomas1192 <gthomas1192@gmail.com>
2017-11-26 11:48:11 -0800

    working sign up email

--- functions/index.js
+++ functions/index.js
@@ -1,34 +1,89 @@
 const functions = require('firebase-functions');
 const nodemailer = require('nodemailer');
 
-// const mailTransport = nodemailer.createTransport({
-//     service: 'gmail',
-//     auth: {
-//         user: gmailEmail,
-//         pass: gmailPassword
-//     }
-// });
 
-
-exports.helloWorld = functions.https.onRequest((request, response) => {
-    response.send("Hello from Firebase!");
-    console.log('HI')
+// Configure the email transport using the default SMTP transport and a GMail account.
+// For Gmail, enable these:
+// 1. https://www.google.com/settings/security/lesssecureapps
+// 2. https://accounts.google.com/DisplayUnlockCaptcha
+// For other types of transports such as Sendgrid see https://nodemailer.com/transports/
+// TODO: Configure the `gmail.email` and `gmail.password` Google Cloud environment variables.
+const gmailEmail = functions.config().gmail.email;
+const gmailPassword = functions.config().gmail.password;
+const mailTransport = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: gmailEmail,
+        pass: gmailPassword
+    }
 });
 
-exports.sendWelcomeEmail = functions.auth.user().onCreate((event) => {
-    console.log('INSIDE SEND WELCOME', event.data)
+// Your company name to include in the emails
+// TODO: Change this to your app or company name to customize the email sent.
+const APP_NAME = 'MotoMechanic';
+
+// [START sendWelcomeEmail]
+/**
+ * Sends a welcome email to new user.
+ */
+// [START onCreateTrigger]
+exports.sendWelcomeEmail = functions.auth.user().onCreate(event => {
+    // [END onCreateTrigger]
+    // [START eventAttributes]
     const user = event.data; // The Firebase user.
 
     const email = user.email; // The email of the user.
-    const displayName = user.displayName; // The display name of the user.
+    const displayName = user.username; // The display name of the user.
     // [END eventAttributes]
-
+    console.log('INSIDE SEND WELCOME EMAIL EVENT')
     return sendWelcomeEmail(email, displayName);
 });
+// [END sendWelcomeEmail]
 
-
+// [START sendByeEmail]
+/**
+ * Send an account deleted email confirmation to users who delete their accounts.
+ */
+// [START onDeleteTrigger]
 exports.sendByeEmail = functions.auth.user().onDelete(event => {
-    console.log('user deleted')
+    // [END onDeleteTrigger]
+    const user = event.data;
+
+    const email = user.email;
+    const displayName = user.displayName;
+
+    return sendGoodbyEmail(email, displayName);
 });
+// [END sendByeEmail]
 
+// Sends a welcome email to the given user.
+function sendWelcomeEmail(email, displayName) {
+    console.log('INSIDE SEND WELCOME EMAIL FUNCTION')
 
+    const mailOptions = {
+        from: `${APP_NAME} <noreply@firebase.com>`,
+        to: email
+    };
+
+    // The user subscribed to the newsletter.
+    mailOptions.subject = `Welcome to ${APP_NAME}!`;
+    mailOptions.text = `Hey ${displayName || ''}! Welcome to ${APP_NAME}. I hope you will enjoy our service.`;
+    return mailTransport.sendMail(mailOptions).then(() => {
+        console.log('New welcome email sent to:', email);
+    });
+}
+
+// Sends a goodbye email to the given user.
+function sendGoodbyEmail(email, displayName) {
+    const mailOptions = {
+        from: `${APP_NAME} <noreply@firebase.com>`,
+        to: email
+    };
+
+    // The user unsubscribed to the newsletter.
+    mailOptions.subject = `Bye!`;
+    mailOptions.text = `Hey ${displayName || ''}!, We confirm that we have deleted your ${APP_NAME} account.`;
+    return mailTransport.sendMail(mailOptions).then(() => {
+        console.log('Account deletion confirmation email sent to:', email);
+    });
+}
