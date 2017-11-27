244ae96df214752f07bb53f9ad06e79334166ecf
Shaun Wahle <shaun.wahle@gmail.com>
2017-11-27 15:10:13 -0800

    adds working on home container

--- src/component/home/index.js
+++ src/component/home/index.js
@@ -1,54 +1,55 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
-import Encyclopedia from '../encyclopedia'
-import './_home.scss'
+import Encyclopedia from '../encyclopedia';
+import './_home.scss';
 import Drawer from 'material-ui/Drawer';
 import MenuItem from 'material-ui/MenuItem';
 import RaisedButton from 'material-ui/RaisedButton';
 import AppBar from 'material-ui/AppBar';
-import NavBar from '../navbar'
+import NavBar from '../navbar';
 
 
 
 
 class Home extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { open: false };
+    constructor(props) {
+        super(props);
+        this.state = { open: false };
 
-    this.handleClose = this.handleClose.bind(this)
-    this.handleToggle = this.handleToggle.bind(this)
-  }
+        this.handleClose = this.handleClose.bind(this);
+        this.handleToggle = this.handleToggle.bind(this);
+    }
 
-  componentDidMount() {
-    console.log('_THIS_DOT_PROPS_', this.props)
-  }
+    componentDidMount() {
+        console.log('_THIS_DOT_PROPS_', this.props);
+    }
 
-  handleToggle() {
+    handleToggle() {
 
-    this.setState({ open: !this.state.open });
-  }
+        this.setState({ open: !this.state.open });
+    }
 
-  handleClose() {
-    this.setState({ open: false });
-  }
+    handleClose() {
+        this.setState({ open: false });
+    }
 
 
-  render() {
-    return (
-      <div className=''>
-        <h1>Hello from the home</h1>
-        <h2>I have no idea what to put here</h2>
+    render() {
+        return ( < div className = 'banner' >
+            <
+            h1 > Hello from the home < /h1>  <
+            h2 > I have no idea what to put here < /h2>
 
-      </div>
-    );
-  }
+            <
+            /div>
+        );
+    }
 }
 
 let mapStateToProps = state => ({
-  auth: state.auth,
-  user: state.user,
+    auth: state.auth,
+    user: state.user,
 });
 
 let mapDispatchToProps = dispatch => ({
